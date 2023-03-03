bb2a526ec590878accb28686616055d15238293b
Syn-dromatic <68112904+Syn-dromatic@users.noreply.github.com>
2023-03-04 01:42:32 +0200

    remove test key setup

--- src/main.rs
+++ src/main.rs
@@ -36,45 +36,6 @@ struct MyWindowHandler {
 }
 
 impl WindowHandler for MyWindowHandler {
-    fn on_key_down(
-        &mut self,
-        _helper: &mut WindowHelper,
-        virtual_key_code: Option<speedy2d::window::VirtualKeyCode>,
-        _scancode: speedy2d::window::KeyScancode,
-    ) {
-
-        let position = Vector2::new(100.0, 100.0);
-        // _helper.set_position_pixels(position);
-        _helper.set_size_scaled_pixels(position);
-
-        let size = Vector2::new(100, 100);
-        // _helper.set_size_pixels(size);
-
-        if let Some(VirtualKeyCode::Up) = virtual_key_code {
-            self.simulation.scale_up();
-        }
-
-        if let Some(VirtualKeyCode::Down) = virtual_key_code {
-            self.simulation.scale_down();
-        }
-
-        if let Some(VirtualKeyCode::W) = virtual_key_code {
-            self.simulation.move_up();
-        }
-
-        if let Some(VirtualKeyCode::S) = virtual_key_code {
-            self.simulation.move_down();
-        }
-
-        if let Some(VirtualKeyCode::D) = virtual_key_code {
-            self.simulation.move_right();
-        }
-
-        if let Some(VirtualKeyCode::A) = virtual_key_code {
-            self.simulation.move_left();
-        }
-    }
-
     fn on_draw(&mut self, helper: &mut WindowHelper, graphics: &mut Graphics2D) {
         self.simulation.simulate(helper, graphics);
     }
